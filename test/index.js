5cfe80498463904a5c0c9acc06780b3e8ea7c4ea
better <shadowan@foxmail.com>
2017-06-09 17:54:36 +0800

    service

--- test/index.js
+++ test/index.js
@@ -18,7 +18,15 @@ var app=angular.module("myApp",[]);
     app.component("pageFooter",{
         templateUrl:"pfooter.html"
     });
-    app.controller("ctrl",["$scope",function ($scope) {
+    app.controller("ctrl",["$scope","$http",function ($scope,$http) {
+        $scope.students=[];
+        $http.get('data.txt')
+            .then(function (data, status, headers, config) {
+                $scope.students = data.students;
+                    console.log(data);
+                    console.log(data.students);
+                    console.log($scope.students);
+                })
         $scope.username="tom";
         $scope.name="anna"
     }])
