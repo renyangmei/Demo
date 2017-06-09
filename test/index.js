879f90d301a1987d0d2b9ff978403648d0ce36a3
better <shadowan@foxmail.com>
2017-06-09 16:02:21 +0800

    component

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var app=angular.module("myApp",[]);
     app.component("pageHeader",{
-        template:"<h2>我是标题，尊敬的{{username}}用户，欢迎访问本系统</h2>",
+        // template:"<h2>我是标题，尊敬的{{username}}用户，欢迎访问本系统</h2>",
+        templateUrl:"pageheader.html",
         controller:function ($scope) {
             $scope.username="小安";
         }
@@ -11,10 +12,10 @@ app.controller("ctrl",["$scope",function ($scope) {
 }])
 
     app.component("pageSlide",{
-        template:"<h2>我是侧边栏</h2>"
+        templateUrl:"pageslide.html"
     });
 
     app.component("pageFooter",{
-        template:"<h2>我是底部</h2>"
+        templateUrl:"pfooter.html"
     })
 
